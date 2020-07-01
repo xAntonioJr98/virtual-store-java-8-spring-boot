d8ff13464cb2354673094be7e3876b9cacc9e4ca
antonio carlos <actorresjunior@gmail.com>
2020-07-01 03:22:26 -0300

    Service Layer and componet registration

--- src/main/java/com/torresantonio/virtualstore/resources/ClientResources.java
+++ src/main/java/com/torresantonio/virtualstore/resources/ClientResources.java
@@ -1,28 +1,35 @@
 package com.torresantonio.virtualstore.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.torresantonio.virtualstore.entities.Client;
+import com.torresantonio.virtualstore.services.ClientService;
 
 @RestController
 @RequestMapping(value="/clients")
 public class ClientResources {
-		
+	
+	@Autowired
+	private ClientService service;
 	
 		@GetMapping
 		public ResponseEntity <List<Client>> findAll(){
-			Client cli1 = new Client(null, "Antonio", "antonio@gmail.com", "51515151", "123");
-			Client cli2 = new Client(null, "Carlos", "carlos@gmail.com", "52525252", "321");
-			List<Client> list = new ArrayList<>();
-			list.addAll(Arrays.asList(cli1, cli2));
+			List<Client> list = service.findAll();
 			return ResponseEntity.ok().body(list);
 			
 		}
+		
+		@GetMapping(value="/{id}")
+		public ResponseEntity<Client> findById(@PathVariable Long id){
+			Client obj = service.findById(id);
+			return ResponseEntity.ok().body(obj);
+			
+}
 }
