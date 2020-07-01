76ea20de2e79a6ea3e453f96bfe45e0dcae0836c
antonio carlos <actorresjunior@gmail.com>
2020-07-01 02:52:15 -0300

    db h2 and test profile/jpa created

--- src/main/java/com/torresantonio/virtualstore/entities/Client.java
+++ src/main/java/com/torresantonio/virtualstore/entities/Client.java
@@ -1,23 +1,38 @@
 package com.torresantonio.virtualstore.entities;
 
 import java.io.Serializable;
+//import java.util.ArrayList;
+//import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+//import javax.persistence.Table;
+
+
+@Entity
+//@Table(name = "TB_Client")
 public class Client implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
 
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
+	//@OneToMany(mappedBy="client")
 	
-	public Client() {
-		
-	}
+	//private List<Order> orders = new ArrayList<>();
 
+	public Client() {
+
+	}
 
 	public Client(Long id, String name, String email, String phone, String password) {
 		super();
@@ -28,56 +43,50 @@ public class Client implements Serializable {
 		this.password = password;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getPhone() {
 		return phone;
 	}
-
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-
 	public String getPassword() {
 		return password;
 	}
-
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/*public List<Order> getOrders() {
+		return orders;
+	}
+*/
 
 	@Override
 	public int hashCode() {
@@ -86,7 +95,6 @@ public class Client implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -104,6 +112,5 @@ public class Client implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
