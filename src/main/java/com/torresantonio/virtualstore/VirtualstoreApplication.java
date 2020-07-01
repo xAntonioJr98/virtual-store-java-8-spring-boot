5bb91edcad1a3ad91e1a307640da1306a1cee47a
antonio carlos <actorresjunior@gmail.com>
2020-07-01 00:29:06 -0300

    Project created

---
+++ src/main/java/com/torresantonio/virtualstore/VirtualstoreApplication.java
@@ -0,0 +1,13 @@
+package com.torresantonio.virtualstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualstoreApplication.class, args);
+	}
+
+}
